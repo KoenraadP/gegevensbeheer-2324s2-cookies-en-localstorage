65986013ec319a583d0c4d66e7974b7746652479
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2024-05-30 10:09:19 +0200

    Add beginning of getcookie

--- scripts/success.js
+++ scripts/success.js
@@ -2,3 +2,23 @@
 
 // huidige cookies opvragen en tonen in console
 console.log(document.cookie);
+
+// welcome alinea koppelen aan variabele
+let welcomeParagraph = document.getElementById("welcome");
+
+// gebruikersnaam ophalen uit userName cookie
+let userName = GetCookie("userName");
+
+// function to get the value
+// of a specific cookie
+function GetCookie(cookieName){
+    // lege string klaarzetten om waarde in op te slaan
+    let cookieValue = "";
+    // alle cookies ophalen
+    // voorbeeld: loggedIn=true; userName=creo; ...
+    let cookies = document.cookie;
+    // cookies apart in array stoppen met split
+    cookies = cookies.split("; ");
+    // even testen
+    console.log(cookies);
+}
