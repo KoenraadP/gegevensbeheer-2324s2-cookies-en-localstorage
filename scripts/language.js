ff96deb206cc3a833b4597ddd55fc022b5a7dfb6
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2024-05-30 11:18:25 +0200

    Add initial code vor language.js

--- scripts/language.js
+++ scripts/language.js
@@ -1,1 +1,21 @@
 "use strict";
+
+// greeting paragraph koppelen aan variabele
+let greetingParagraph = document.getElementById("greeting");
+
+// alle language links verzamelen
+// alle elementen verzamelen met de class language-link
+let languageLinks = document.querySelectorAll(".language-link");
+
+// alle links hetzelfde click event geven
+languageLinks.forEach(element => {
+    element.addEventListener("click",function(e){
+        // preventDefault --> pagina niet herladen bij klikken
+        e.preventDefault();
+        // gekozen taal opslaan in variabele
+        // ophalen via id van aangeklikte element (e)
+        let chosenLanguage = e.target.id;
+        // even testen
+        console.log(chosenLanguage);
+    })
+});
